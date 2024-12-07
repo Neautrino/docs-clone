e0c4c00a0d8a255fe0617e8d23a03da5ad1b4cab
Neautrino <ssubhendu988@gmail.com>
2024-12-07 18:30:19 +0000

    fixed ruler

--- src/app/documents/[documentId]/Ruler.tsx
+++ src/app/documents/[documentId]/Ruler.tsx
@@ -140,13 +140,13 @@ const Marker = ({
             onDoubleClick={onDoubleClick}
         >
             <FaCaretDown className="absolute left-1/2 top-0 h-full fill-blue-500 transform -translate-x-1/2" />
-            <div className="absolute left-1/2 top-4 transform -translate-x-1/2 duration-150"
+            <div className="absolute left-1/2 top-4 transform -translate-x-1/2"
             style={{
                 height: "100vh",
                 width: "1px",
                 transform: "scaleX(0.5)",
                 backgroundColor: "#3b72f6",
-                
+                display: isDragging ? "block" : "none"
             }}/>
         </div>
     )
