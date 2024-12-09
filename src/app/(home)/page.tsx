9f44049bb8fa50155db9ca9b332db93d12cdc07c
Neautrino <ssubhendu988@gmail.com>
2024-12-09 18:33:06 +0000

    connect db and define schema

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,19 @@
+'use client';
+
 import React from 'react'
 import Navbar from './Navbar'
-import Link from 'next/link'
 import TemplatesGallary from './TemplatesGallary'
+import { useQuery } from 'convex/react';
+import { api } from '../../../convex/_generated/api';
 
 function Home() {
+
+  const documents = useQuery(api.documents.get);
+
+  if(documents === undefined) {
+    return <div>Loading...</div>
+  }
+
   return (
     <div className='min-h-screen flex flex-col'>
       <div className='fixed top-0 left-0 right-0 z-10 h-16 bg-white p-4'>
@@ -11,6 +21,9 @@ function Home() {
       </div>
       <div className="mt-16">
         <TemplatesGallary />
+        {documents?.map((document) => (
+          <span key={document._id}> {document.title}</span>
+        ))}
       </div>
     </div>
   )
